e53d21277d01b414a373685ad2149130b8631740
Blair Bristow <blairhbristow@gmail.com>
2022-03-21 17:36:03 -0400

    Edit solutions for part 1 and 2

--- exercise.js
+++ exercise.js
@@ -1,49 +1,24 @@
-const candies = [
-    {
-        id: 1,
-        description: "Dark Chocolate Crunchies",
-        price: 4.29
-    }, {
-        id: 2,
-        description: "Mint Wafers",
-        price: 1.09
-    }, {
-        id: 3,
-        description: "Peppermint Poppers",
-        price: 2.38
-    }];
-
-function getItemById(num) {
-    let candy = candies.find(snack => snack.id === num);
-    console.log(candy);
+function getItemById(items, id) {
+    return items.find(item => item.id === id)
 }
 
-const sales = [
-    {
-        itemId: 1,
-        quantity: 2
-    }, {
-        itemId: 2,
-        quantity: 2
-    }, {
-        itemId: 1,
-        quantity: 2
-    }, {
-        itemId: 3,
-        quantity: 3
-    }];
+function buildTransactions(sales, items) {
+    return sales.map(sale => {
+        const itemId = sale.itemId
+        const item = getItemById(items, itemId)
 
+        return {
+            ...sale,
+            description: item.description,
+            price: item.price,
 
-function buildTransactions(num) {
-    let salesDescribed = sales.map(sale => {
-        
-    });
-    console.log(salesDescribed);
+        }
+    })
 }
 
 module.exports = {
     // Uncomment these as you write them
-    // getItemById,
-    // buildTransactions,
+    getItemById,
+    buildTransactions,
     // getTransactionsByItemDescription
 }
