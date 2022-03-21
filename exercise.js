09b4e38462e85b94bc56433dfa796931fe83f301
Blair Bristow <blairhbristow@gmail.com>
2022-03-21 15:51:48 -0400

    Add candies array and getItemById function

--- exercise.js
+++ exercise.js
@@ -1,4 +1,22 @@
-// Your code goes here
+const candies = [
+{
+    id: 1,
+    description: "Dark Chocolate Crunchies",
+    price: 4.29
+},{
+    id: 2,
+    description: "Mint Wafers",
+    price: 1.09
+},{
+    id: 3,
+    description: "Peppermint Poppers",
+    price: 2.38
+}];
+
+function getItemById(id){
+    let candy = candies.find(snack => snack.id === id);
+    console.log(candy);
+}
 
 module.exports = {
     // Uncomment these as you write them
