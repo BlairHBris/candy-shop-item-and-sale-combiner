f59a701b3b974a5631bc4958d84fdd01d2ab1900
Blair Bristow <blairhbristow@gmail.com>
2022-03-21 16:45:27 -0400

    Fix type and add const sales

--- exercise.js
+++ exercise.js
@@ -1,21 +1,44 @@
 const candies = [
-{
-    id: 1,
-    description: "Dark Chocolate Crunchies",
-    price: 4.29
-},{
-    id: 2,
-    description: "Mint Wafers",
-    price: 1.09
-},{
-    id: 3,
-    description: "Peppermint Poppers",
-    price: 2.38
-}];
+    {
+        id: 1,
+        description: "Dark Chocolate Crunchies",
+        price: 4.29
+    }, {
+        id: 2,
+        description: "Mint Wafers",
+        price: 1.09
+    }, {
+        id: 3,
+        description: "Peppermint Poppers",
+        price: 2.38
+    }];
 
-function getItemById(id){
-    let candy = candies.find(snack => snack.id === id);
+function getItemById(num) {
+    let candy = candies.find(snack => snack.id === num);
     console.log(candy);
+}
+
+const sales = [
+    {
+        itemId: 1,
+        quantity: 2
+    }, {
+        itemId: 2,
+        quantity: 2
+    }, {
+        itemId: 1,
+        quantity: 2
+    }, {
+        itemId: 3,
+        quantity: 3
+    }];
+
+
+function buildTransactions(num) {
+    let salesDescribed = sales.map(sale => {
+        
+    });
+    console.log(salesDescribed);
 }
 
 module.exports = {
